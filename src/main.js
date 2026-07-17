46aaf8eccf82b17047136bdf36c4c2b6bf2492e7
Plus <channellast061@gmail.com>
2026-07-17 12:37:52 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, 'downl.ico'),
+    icon: path.join(__dirname, 'female_woman_girl_person_user_client_1470.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
@@ -26,8 +26,3 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
-🧠File.preload.js
-
-window.addEventListener('DOMContentLoaded', () => {
-  console.log('Pokejumper loaded');
-});
