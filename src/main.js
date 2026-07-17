61d9c3991259c1b57785880f9a4b895e6f55eef4
Plus <channellast061@gmail.com>
2026-07-17 12:31:58 -0400

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    icon: path.join(__dirname, 'downl.ico'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  win.loadURL('https://www.fedpromptly.com');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+🧠File.preload.js
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('Pokejumper loaded');
+});
